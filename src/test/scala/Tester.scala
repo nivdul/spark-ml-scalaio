2ea6a048709827dfed87d36b115ec9dd6d613fc7
samklr <samklr@icloud.com>
2014-10-13 14:04:55 +0000

    Fix Test

--- src/test/scala/Tester.scala
+++ src/test/scala/Tester.scala
@@ -1,16 +1,18 @@
 import com.fasterxml.jackson.annotation.JsonUnwrapped
 import com.google.common.annotations.VisibleForTesting
-import sparkapps.SparkApp1
+import sparkapps.SparkApp
 
-/**
- * Created by apache on 7/20/14.
- */
+
+
+
+
+
 class Tester {
 
   @org.junit.Test
   def test(){
-    //very simple unit test.  we will improve it eventually.
-    SparkApp1.main(Array("1"));
+    //Simple Test
+    SparkApp.main(Array("1"));
 
   }
 }
